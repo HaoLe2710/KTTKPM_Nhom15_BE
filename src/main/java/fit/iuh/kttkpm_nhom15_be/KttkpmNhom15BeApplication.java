5434d4ac9eb19520aa153b4c339498d4832d15e5
Minhduy407 <minhdii1510@gmail.com>
2026-04-17 22:52:35 +0700

    chore(infra): remove Eureka discovery client and cleanup configuration

--- src/main/java/fit/iuh/kttkpm_nhom15_be/KttkpmNhom15BeApplication.java
+++ src/main/java/fit/iuh/kttkpm_nhom15_be/KttkpmNhom15BeApplication.java
@@ -3,14 +3,12 @@ package fit.iuh.kttkpm_nhom15_be;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableDiscoveryClient
 public class KttkpmNhom15BeApplication {
     @PostConstruct
     public void init() {
